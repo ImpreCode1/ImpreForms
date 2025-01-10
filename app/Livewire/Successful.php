5b759fbfc5a92c2e6c5b54afc78916add5f428bb
ImpreCode1 <soporteit@impresistem.com>
2025-01-10 17:15:15 -0500

    feat: :art: Design and implementation of the Shipped component successfully

---
+++ app/Livewire/Successful.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Successful extends Component
+{
+    public function render()
+    {
+        return view('livewire.successful');
+    }
+}
