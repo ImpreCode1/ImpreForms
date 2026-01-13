78bac309a7ac7439f80ea0414b9bc6ed622b1ac5
SebastianOG509 <seortizg@unal.edu.co>
2026-01-13 14:19:32 -0500

    Actualizar la configuración de la sesión para usar la variable de entorno para la expiración de la sesión al cerrar

--- config/session.php
+++ config/session.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Str;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Session Driver
@@ -33,7 +32,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => false,
+    'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -210,5 +209,4 @@ return [
     */
 
     'partitioned' => false,
-
 ];
