ba538fec7c3db80d4e24f01f35ef0908dec906bb
SebastianOG509 <seortizg@unal.edu.co>
2026-04-29 16:53:55 -0500

    Agregar registro de salida al comando 'eliminar:registros-vencidos' en el cron para mejorar la supervisión.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // Ejecutar el comando todos los años
-        $schedule->command('eliminar:registros-vencidos')->weeklyOn(6, '02:00');
+        $schedule->command('eliminar:registros-vencidos')->weeklyOn(6, '02:00')->appendOutputTo(storage_path('logs/cron.log'));
     }
 
     /**
