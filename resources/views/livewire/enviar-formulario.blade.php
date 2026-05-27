8bfc470f169e3d7f14a85f54f90d6312a85a22e0
DanielFeli2006 <177273086+DanielFeli2006@users.noreply.github.com>
2026-05-27 12:43:52 -0500

    feat: agregar SweetAlert2 como dependencia y actualizar formularios para su uso

--- resources/views/livewire/enviar-formulario.blade.php
+++ resources/views/livewire/enviar-formulario.blade.php
@@ -1099,7 +1099,7 @@
             </div>
         </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 <script>
     document.addEventListener('livewire:init', function () {
         Livewire.on('validation-error', (data) => {
